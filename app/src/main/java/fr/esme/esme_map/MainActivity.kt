e110ed6d2421339dd8f21bbe03c3c2e7d6744c7e
jean-paul.jacquot <jacquot.jpaul@gmail.com>
2020-11-17 13:13:25 +0100

    initial commit

---
+++ app/src/main/java/fr/esme/esme_map/MainActivity.kt
@@ -0,0 +1,11 @@
+2package fr.esme.esme_map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
