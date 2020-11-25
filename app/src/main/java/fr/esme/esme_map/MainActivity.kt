7e757b33f721d7533a0f2f7c7edbd6c87bb1928b
jean-paul.jacquot <jacquot.jpaul@gmail.com>
2020-11-25 16:07:49 +0100

    wip

--- app/src/main/java/fr/esme/esme_map/MainActivity.kt
+++ app/src/main/java/fr/esme/esme_map/MainActivity.kt
@@ -1,12 +1,14 @@
 package fr.esme.esme_map
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -38,7 +40,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         //BUTTON
         var button = findViewById<FloatingActionButton>(R.id.button)
-
+        var imageView = findViewById<ImageView>(R.id.imageView)
         //CallBack
         button.setOnClickListener {
 
