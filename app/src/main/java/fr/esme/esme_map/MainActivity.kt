5cc464e92c5844f271ab0adfec8aff1876df4e28
jean-paul.jacquot <jacquot.jpaul@gmail.com>
2020-11-25 15:13:47 +0100

    wip

--- app/src/main/java/fr/esme/esme_map/MainActivity.kt
+++ app/src/main/java/fr/esme/esme_map/MainActivity.kt
@@ -2,6 +2,8 @@ package fr.esme.esme_map
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import fr.esme.esme_map.interfaces.UserInterface
 import fr.esme.esme_map.model.User
@@ -13,10 +15,32 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
-        val user = User("JP") //TODO Call database login user
+        val user = User("JP") //Me
         val userInterface: UserInterface? = UserImplementation(user)
+
+
+        //BUTTON
+        var button = findViewById<Button>(R.id.button)
+        var helloWorldTextView = findViewById<TextView>(R.id.helloWorldTextView)
+
+        //CallBack
+        button.setOnClickListener {
+
+            var POIString: String = ""
+            userInterface?.getPOIs()?.forEach {
+
+                POIString += "\n " + it.name +"   longitude = " +
+                        it.position.longitude +
+                        " latitude =" + it.position.latitude
+
+            }
+
+            helloWorldTextView.text = POIString
+
+        }
 
         // Show user Position
         Log.d(
@@ -29,7 +53,7 @@ class MainActivity : AppCompatActivity() {
         userInterface?.getPOIs()?.forEach {
             Log.d(
                 TAG,
-                "Show user POI : longitude = " +
+                "Show user Run : longitude = " +
                         it.position.longitude +
                         " latitude =" + it.position.latitude
             )
