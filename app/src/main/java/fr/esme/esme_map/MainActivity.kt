ef6651cf25453ee7ca901d9092f1ba084cc9ec1e
jean-paul.jacquot <jacquot.jpaul@gmail.com>
2020-12-01 16:37:17 +0100

    fin du cours

--- app/src/main/java/fr/esme/esme_map/MainActivity.kt
+++ app/src/main/java/fr/esme/esme_map/MainActivity.kt
@@ -1,29 +1,51 @@
 package fr.esme.esme_map
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import fr.esme.esme_map.interfaces.UserInterface
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.squareup.picasso.Picasso
+import fr.esme.esme_map.model.Category
 import fr.esme.esme_map.model.POI
 import fr.esme.esme_map.model.Position
+import fr.esme.esme_map.model.User
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private val TAG = MainActivity::class.qualifiedName
     private lateinit var mMap: GoogleMap
     private lateinit var viewModel: MainActivityViewModel
+    private var isFriendShow = false
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         viewModel.getPOIFromViewModel()
         viewModel.getPositionFromViewModel()
+
+        mMap.setOnMapClickListener {
+
+            showPOI(
+                POI(
+                    User(""),
+                    "",
+                    0,
+                    Position(it.latitude, it.longitude),
+                    Category("", Color())
+                )
+            )
+
+
+        }
 
     }
 
@@ -34,6 +56,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         setContentView(R.layout.activity_main)
 
+        //button
+        findViewById<FloatingActionButton>(R.id.showFriendsButton).setOnClickListener {
+            manageUserVisibility()
+        }
+
+
         //MAP
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -42,7 +70,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         viewModel = MainActivityViewModel()
 
         viewModel.poisLiveData.observe(this, { listPOIs ->
-            showPOI(listPOIs)
+            showPOIs(listPOIs)
         })
 
         viewModel.myPositionLiveData.observe(this, { position ->
@@ -53,11 +81,22 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     //TODO show POI
-    fun showPOI(POIs: List<POI>) {
+    fun showPOIs(POIs: List<POI>) {
         POIs?.forEach {
             val poiPos = LatLng(it.position.latitude, it.position.longitude)
             mMap.addMarker(MarkerOptions().position(poiPos).title(it.name))
         }
+    }
+
+    fun showPOI(poi: POI) {
+        mMap.addMarker(
+            MarkerOptions().position(
+                LatLng(
+                    poi.position.latitude,
+                    poi.position.longitude
+                )
+            ).title(poi.name)
+        )
     }
 
     //TODO show MyPosition
@@ -69,7 +108,22 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
 
     //TODO show Travel
+
     //TODO show USer
+    fun manageUserVisibility() {
+
+        if (isFriendShow) {
+            isFriendShow = false
+            findViewById<ImageView>(R.id.friend1).visibility = View.INVISIBLE
+            findViewById<ImageView>(R.id.friend2).visibility = View.INVISIBLE
+            findViewById<ImageView>(R.id.friend3).visibility = View.INVISIBLE
+        } else {
+            isFriendShow = true
+            findViewById<ImageView>(R.id.friend1).visibility = View.VISIBLE
+            findViewById<ImageView>(R.id.friend2).visibility = View.VISIBLE
+            findViewById<ImageView>(R.id.friend3).visibility = View.VISIBLE
+        }
+    }
 
     override fun onStart() {
         super.onStart()
@@ -80,6 +134,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume")
+
+        //TODO mettre une photo a mes potte
+        Picasso.get().load("https://assets.stickpng.com/images/5a8efbf0b15d5c051b36901e.png")
+            .into(findViewById<ImageView>(R.id.friend1))
+        Picasso.get().load("https://assets.stickpng.com/images/5a8efbf0b15d5c051b36901e.png")
+            .into(findViewById<ImageView>(R.id.friend2))
+        Picasso.get().load("https://assets.stickpng.com/images/5a8efbf0b15d5c051b36901e.png")
+            .into(findViewById<ImageView>(R.id.friend3))
 
     }
 
